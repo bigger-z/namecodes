f9942ae7e27a7b0f21f9f02fee7ee762b9bdfbde
simefield <me@richardsime.com>
2026-09-10 13:13:31 +1200

    Extend word list

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { WORDS } from "./words.js?v=11";
+import { WORDS } from "./words.js?v=14";
 
 const boardEl = document.getElementById("board");
 const keyBtn = document.getElementById("key-btn");
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "codenames-v47";
+const CACHE = "codenames-v50";
 const ASSETS = [
   "./",
   "./index.html",
