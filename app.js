501a24c7c9b0537666c93d51d59e771c57f9f4a8
simefield <me@richardsime.com>
2026-09-10 16:18:04 +1200

    Remove more uncommon words

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { WORDS } from "./words.js?v=16";
+import { WORDS } from "./words.js?v=20";
 
 const boardEl = document.getElementById("board");
 const keyBtn = document.getElementById("key-btn");
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "codenames-v52";
+const CACHE = "codenames-v56";
 const ASSETS = [
   "./",
   "./index.html",
